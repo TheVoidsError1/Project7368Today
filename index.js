1923c42799d7902cee6daec97d2ebdb4d372089b
TheVoidsError1 <hgfrttrgam@gmail.com>
2025-03-07 11:55:31 +0700

    add static file serving and new routes for hello-world, name, and day

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-let test;
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("/hello-world", (req, res) => {
+    res.send("Hello...");
+  });
+  
+app.get("/Name", (req, res) => {
+    res.send(`
+      <html>
+        <head>
+          <link rel="stylesheet" type="text/css" href="/style.css">
+        </head>
+        <body>
+          <div class="name">Pajongsak <br> Wirithpon</div>
+        </body>
+      </html>
+    `);
+  });
 
+  app.get("/Day", (req, res) => {
+    res.send("ดีง้าบบบ วันศุกร์...");
+  });
+  
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
